a3d5f532b1125fa44ad47ef414566f4156d756fe
Abdelrahman <apoeljzzar@gmail.com>
2025-03-01 15:53:39 +0200

    Created UserModel & Enum Types

---
+++ src/DB/Models/user.model.js
@@ -0,0 +1,69 @@
+import mongoose, { Schema, model, Types } from "mongoose";
+import { hash } from "../../utils/hashing/hash.js";
+import * as enumTypes from "../enumTypes.js";
+
+const userSchema = new Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+
+        provider: {
+            type: String,
+            enum: Object.values(enumTypes.providersTypes),
+            default: enumTypes.providersTypes.System,
+        },
+        gender: {
+            type: String,
+            enum: Object.values(enumTypes.genderType),
+            default: enumTypes.genderType.Male,
+        },
+        role: {
+            type: String,
+            enum: Object.values(enumTypes.roleType),
+            default: enumTypes.roleType.User,
+        },
+
+        DOB: { type: Date, required: true },
+        mobileNumber: { type: String },
+        isConfirmed: { type: Boolean, default: false },
+        deletedAt: { type: Date },
+        bannedAt: { type: Date },
+        updatedBy: { type: Types.ObjectId, ref: "User" },
+        changeCredentialTime: { type: Date },
+
+        profilePic: { type: String },
+        coverPic: { type: String },
+
+        OTP: [
+            {
+                code: { type: String, required: true },
+                type: { type: String, enum: Object.values(enumTypes.OTPType) },
+                createdAt: { type: Date, default: Date.now, expires: 600 }, // 10 min expiry
+            }
+        ],
+    },
+    { timestamps: true }
+);
+
+// Virtual field for userName
+userSchema.virtual("userName").get(function () {
+    return `${this.firstName} ${this.lastName}`;
+});
+
+// Hash password before saving
+userSchema.pre("save", function (next) {
+    if (this.isModified("password")) {
+        this.password = hash({ plainText: this.password });
+    }
+    next();
+});
+
+// Indexing for performance
+userSchema.index({ email: 1 }, { unique: true });
+
+const UserModel = model("User", userSchema);
+
+export default UserModel;
+
---
+++ src/DB/enumTypes.js
@@ -0,0 +1,20 @@
+
+export const providersTypes = {
+    System: "System",
+    Google: "Google",
+};
+
+export const genderType = {
+    Male: "Male",
+    Female: "Female"
+};
+
+export const roleType = {
+    Admin: "Admin",
+    User: "User",
+};
+
+export const OTPType = {
+    confirmEmail: "confirmEmail",
+    forgetPassword: "forgetPassword",
+};
